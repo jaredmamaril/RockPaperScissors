18233c6f3dbef1470e15de211732f05a7a596bfe
mattjaredmamaril <tytjared@gmail.com>
2026-04-10 20:54:53 -0500

    Add getComputerChoice function to script.js for game logic

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hello, World!");
+
+function getComputerChoice() {
+    const choices = ["rock", "paper", "scissors"];
+    const randomIndex = Math.floor(Math.random() * choices.length);
+
+    return choices[randomIndex];
+}
